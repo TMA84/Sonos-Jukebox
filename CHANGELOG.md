402d538f984933f7690d691103723964d62eb318
TMA <dops@placeholder.systems>
2026-01-16 17:27:20 +0100

    v2.2.17 - Implement virtual keyboard on home page
    - Added full QWERTY keyboard layout with proper event handling
    - Implemented backspace, space, and hide functionality
    - Added keyboard styling with touch-friendly buttons
    - Fixed keyboard integration with search field

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.2.17] - 2026-01-16
+
+### 🐛 Bug Fixes
+
+- **Virtual keyboard** - Implemented missing virtual keyboard functionality on home page
+- **Keyboard events** - Added backspace, tab, and hide event emitters
+- **Keyboard UI** - Created full QWERTY keyboard layout with proper styling
+- **Search integration** - Keyboard now properly inputs into search field
+
 ## [2.2.16] - 2026-01-12
 
 ### 🐛 Bug Fixes
--- src/app/virtual-keyboard/virtual-keyboard.component.ts
+++ src/app/virtual-keyboard/virtual-keyboard.component.ts
@@ -3,9 +3,28 @@ import { Component, EventEmitter, Output, Input } from '@angular/core';
 @Component({
   selector: 'app-virtual-keyboard',
   templateUrl: './virtual-keyboard.component.html',
-  styleUrls: ['./virtual-keyboard.component.scss']
+  styleUrls: ['./virtual-keyboard.component.scss'],
 })
 export class VirtualKeyboardComponent {
   @Input() isVisible = false;
   @Output() keyPress = new EventEmitter<string>();
+  @Output() backspace = new EventEmitter<void>();
+  @Output() tab = new EventEmitter<void>();
+  @Output() hide = new EventEmitter<void>();
+
+  onKeyPress(key: string) {
+    this.keyPress.emit(key);
+  }
+
+  onBackspace() {
+    this.backspace.emit();
+  }
+
+  onTab() {
+    this.tab.emit();
+  }
+
+  onHide() {
+    this.hide.emit();
+  }
 }
